a1fafab0c66079d513b82ebeb36ff1f07b44e01d
Aric <74705062+ninjarogue@users.noreply.github.com>
2021-12-13 01:33:47 +0800

    organizing again

--- init.lua
+++ init.lua
@@ -1,62 +1,8 @@
 
-
-
--- Hide lsp diagnostics virtual text
-vim.diagnostic.config({virtual_text = false})
-
-vim.nvim_tui_enable_true_color = true
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
-
---Map blankline
-vim.g.indent_blankline_char = '┊'
-vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
-vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
-vim.g.indent_blankline_char_highlight = 'LineNr'
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-
-
-
-local nightfox = require('nightfox')
-nightfox.setup{
-  styles = {
-    comments = 'italic',
-    keywords = 'bold',
-    functions = 'italic,bold'
-  }
-}
-nightfox.load()
-
-
-
-vim.g.lightline = {
-  colorscheme = 'nightfox',
-
-  active = {
-    left = {
-      { 'mode', 'paste' },
-      {
-        'gitbranch',
-        'readonly',
-        'filename',
-        'modified'
-      }
-    }
-  },
-
-  component_function = { gitbranch = 'fugitive#head' },
-}
-
-
-
--- vim.g.gruvbox_flat_style = 'dark'
--- vim.g.gruvbox_dark_sidebar = true
--- vim.g.gruvbox_sidebars = { "qf", "terminal", "packer" }
--- vim.g.gruvbox_colors = {
---   fg = "#D4D298",
--- }
+require('plugins')
+require('globals')
+require('options')
+require('mappings')
 
 -- load colorscheme after configuration
 -- vim.cmd [[colorscheme gruvbox-flat]]
@@ -75,10 +21,4 @@ vim.api.nvim_exec(
 
   false
 )
-
-
-
-require('plugins')
-require('options')
-require('mappings')
 
