c408ac0298205109479504bc4e89fc4b0f5b2bc4
Aric <74705062+ninjarogue@users.noreply.github.com>
2022-01-21 19:15:07 +0800

    latest general updates

--- init.lua
+++ init.lua
@@ -3,18 +3,21 @@ require('globals')
 require('options')
 require('mappings')
 
--- load colorscheme after configuration
- -- vim.g.gruvbox_flat_style = 'dark'
- -- vim.g.gruvbox_dark_sidebar = true
- -- vim.g.gruvbox_sidebars = { "qf", "terminal", "packer" }
- -- vim.g.gruvbox_colors = {
- --   fg = "#D4D298",
- -- }
---
 
-require('onedark').setup {
-    style = 'darker'
-}
 
-vim.cmd('colorscheme onedark')
+-- require('onedark').setup {
+--     style = 'darker'
+-- }
+
+
+
+--load colorscheme after configuration
+  vim.g.gruvbox_flat_style = 'dark'
+  vim.g.gruvbox_dark_sidebar = true
+  vim.g.gruvbox_sidebars = { "qf", "terminal", "packer" }
+  vim.g.gruvbox_colors = {
+    fg = "#D4D298",
+  }
+
+vim.cmd('colorscheme gruvbox-flat')
 
