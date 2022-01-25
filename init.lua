55097c17282d4e0fc971d65d90d24c08d227f541
Aric Jiang <aricjiang@Arics-MacBook-Pro.local>
2022-01-26 00:54:59 +0800

    add new theme

--- init.lua
+++ init.lua
@@ -5,19 +5,7 @@ require('mappings')
 
 
 
--- require('onedark').setup {
---     style = 'darker'
--- }
-
-
 
 --load colorscheme after configuration
-  vim.g.gruvbox_flat_style = 'dark'
-  vim.g.gruvbox_dark_sidebar = true
-  vim.g.gruvbox_sidebars = { "qf", "terminal", "packer" }
-  vim.g.gruvbox_colors = {
-    fg = "#D4D298",
-  }
-
-vim.cmd('colorscheme gruvbox-flat')
+vim.cmd('colorscheme onedark')
 
