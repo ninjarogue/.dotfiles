a8f376c81342877827544a895311177debe8fecd
Aric <74705062+ninjarogue@users.noreply.github.com>
2022-06-15 11:13:14 +0800

    add new telescope picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -187,5 +187,7 @@ return function()
   vim.api.nvim_set_keymap('n', '<space>fb', [[<cmd>lua require 'telescope'.extensions.file_browser.file_browser({cwd = require('telescope.utils').buffer_dir()})<CR>]], {noremap = true})
 
   vim.api.nvim_set_keymap('n', '<leader>dg', [[<cmd>lua require('telescope.builtin').diagnostics()<CR>]], { noremap = true, silent = true })
+
+  vim.api.nvim_set_keymap('n', '<leader>st', [[<cmd>lua require('telescope.builtin').git_stash()<CR>]], { noremap = true, silent = true })
 end
 
